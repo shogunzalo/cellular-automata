894cb49fad0cca4f2b71513ee6728e1c8d2d7487
Gonzalo Rodriguez <g_rod@live.cl>
2022-06-28 06:09:46 -0400

    Code refactor

--- automata.js
+++ automata.js
@@ -2,12 +2,12 @@ const { getRules, printDelayed, getCells } = require("./helpers")
 
 const ruleNumber = process.env.RULE || 30
 const generations = process.env.GENERATIONS || 70;
-const canvasWidth = 70;
 
+const blackSquare = "\033[40m  \033[0m"
+const whiteSquare = "\033[47m  \033[0m"
+const canvasWidth = 70;
 const zero = "0"
 const one = "1"
-const whiteSquare = "\033[47m  \033[0m"
-const blackSquare = "\033[40m  \033[0m"
 const timeout = 100;
 
 
@@ -17,7 +17,6 @@ const rules = getRules(ruleNumber);
 let previousState = new Array(canvasWidth).fill(zero);
 previousState[startPoint] = one;
 
-let newState = [];
 
 let toDraw = new Array(canvasWidth).fill(whiteSquare);
 toDraw[startPoint] = blackSquare;
@@ -29,18 +28,16 @@ const draw = () => {
 
         printDelayed(toDraw.join(''))
 
-        newState = new Array(canvasWidth).fill(zero);
+        const newState = new Array(canvasWidth).fill(zero);
         toDraw = new Array(canvasWidth).fill(whiteSquare);
 
         for (let j = 0; j < canvasWidth; j++) {
-
             let cells = getCells(previousState, j, canvasWidth)
+            let ruleResult = rules[cells]
 
-            let rule = rules[cells]
+            newState[j] = ruleResult || zero
 
-            newState[j] = rule || zero
-
-            if (rule === one) {
+            if (ruleResult === one) {
                 toDraw[j] = blackSquare
             }
         }
